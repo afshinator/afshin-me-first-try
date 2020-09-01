9d2e642fe9759ffe170925696a817378f8cc3a6c
Afshin Mokhtari <afshinator@hotmail.com>
2020-08-31 18:17:07 -0700

    Add Lato and Yellowtail custom fonts from google

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   purge: [],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'lato': ['lato', 'sans-serif'],
+        'yt': ['yellowtail', 'sans']
+      },
+    },
   },
   variants: {},
   plugins: [],
